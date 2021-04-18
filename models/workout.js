79e1901c7d918b4498a962d6e8e67439c5f193eb
The Noise <leespaulding25@gmail.com>
2021-04-18 13:18:39 -0400

    added to workout.js

--- models/workout.js
+++ models/workout.js
@@ -1,2 +1,32 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+
+const Newworkout = new Schema({
+    day:{
+    type: Date,
+    default: Date.now
+    },
+    exercise:[
+    {
+     type:{
+        type:{String,
+        trim: true,
+        required: "Name of Exercise"
+    },
+    },
+    duration: {
+        type:Number,
+        required: " length of workout"
+    },
+    distance:{
+        type:Number
+    },
+    reps: {
+        type:Number
+    },
+    weight:{
+        type:Number
+    }
+},
+]
+});
