05266da6e45cde43847040149fcfd2a21e970f53
The Noise <leespaulding25@gmail.com>
2021-04-18 14:54:04 -0400

    created workoutSchema function to input in the stats.js for the charts

--- models/workout.js
+++ models/workout.js
@@ -1,5 +1,17 @@
 const mongoose = require("mongoose");
+const { Workout, Workout } = require(".");
 const Schema = mongoose.Schema;
+
+ const { Schema } = mongoose;
+
+ const schemaOptions ={
+     toObject: {
+         virtuals:true,
+     },
+     toJson: {
+         virtuals: true,
+     },
+ };
 
 const WorkoutSchema = new Schema({
     day:{
@@ -27,6 +39,42 @@ const WorkoutSchema = new Schema({
     weight:{
         type:Number
     }
-},
 ]
+})
+
+
+WorkoutSchema.virtual("totalDuration").get(function(){
+    let totalDuration = 0;
+    for (let i=0; i,this.exercises.length; i++) {
+        totalDuration += this.exercise[i].duration;
+    }
+    return totalDuration;
 });
+
+WorkoutSchema.virtual("dayofWeek").get(function (){
+    const days ={
+        0: "Sunday",
+        1: "Monday",
+        2:"Tuesday",
+        3:"Wensday",
+        4:"Thursday",
+        5:"Friday",
+        6:"Saturday",
+    };
+const dayofWeek = this.day.getDay();
+return dayNames{dayofWeek};
+});
+
+// WorkoutSchema.virtual("totalWeight").get(function(){
+//     let totalWeight = 0;
+//     for (let i=0; i,this.exercises.length; i++) {
+//         if (typeof this.exercises[i].weight === "number") {
+//             totalWeight += (this.exercise[i].weight * this.exercisea[i].reps * this.ecercise[i].sets);
+//         }
+//     }
+//     return totalWeight;
+// })
+// const Workout =mongoose.model("workout", WorkoutSchema);
+
+module.exports =Workout;
+
